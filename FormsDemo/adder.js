cf49940aed824e5e9e9ee13ff0f38158cef5b807
olanmj <olanm@stockton.edu>
2018-01-30 09:49:26 -0500

    Simple adder implemented

---
+++ FormsDemo/adder.js
@@ -0,0 +1,9 @@
+﻿var add = function () {
+    var total = Number(document.querySelector("#op1").value) +
+        Number(document.querySelector("#op2").value);
+    document.querySelector("#sum").innerHTML = total;
+}
+
+window.onload = function () {
+    document.querySelector("#btn").onclick = add;
+}
